0c3fd4778404154f4d86d98eff076b5beaf1f0aa
Michael Crombie <mcrombie1994@gmail.com>
2019-12-26 18:11:26 -0500

    Some updates in preparation for interview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function padWithZero(num) {
-  let result = num;
-  if (num < 10) { result = "0" + num.toString() }
-  return result;
+const DEFAULT = {
+  started: false, //has the clock been started yet?
+  running: false, //has the clock been puased yet?
+  type: "Session", //type: setting || break
+  breakLength: 5,
+  sessionLength: 25,
+  remainingMinutes: 25,
+  remainingSeconds: 0
 }
+
+/* --------------------------------- */
+/* APPLICATION - POMODORO CLOCK  */
+/* --------------------------------- */
 
 class PomodoroClock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      started: false,
-      running: false,
-      type: "Session",
-      breakLength: 5,
-      sessionLength: 25,
-      remainingMinutes: 25,
-      remainingSeconds: 0
-    }
+    this.state = DEFAULT
+    // OUTDATED BINDINGS?
     this.runTimer = this.runTimer.bind(this);
     this.startOrStopSession = this.startOrStopSession.bind(this);
     this.switchType = this.switchType.bind(this);
@@ -30,8 +30,11 @@ class PomodoroClock extends React.Component {
     this.reset = this.reset.bind(this);
   }
   runTimer() {
+    // WHEN RUNNING DECREMENT CLOCK TIME
     if (this.state.running) {
+      //DECREMENT REMAINING SECONDS TO 0
       if (this.state.remainingSeconds === 0) {
+        // DECREMENT REMAINING MINUTES TO 0, PLAY SOUND, SWITCH SESSION TYPE
         if (this.state.remainingMinutes === 0) {
           document.getElementById("beep").play()
           this.switchType();
@@ -49,15 +52,15 @@ class PomodoroClock extends React.Component {
     }
   }
   startOrStopSession() {
-    if (!this.state.started) {
-      window.setInterval(this.runTimer, 1000)
-    }
+    // PLAY AND PAUSE
+    if (!this.state.started) { window.setInterval(this.runTimer, 1000)}
     this.setState({
       started: true,
       running: !this.state.running
     })
   }
   switchType() {
+    // SET SESSION TYPE TO ITS COUNTERPART
     if (this.state.type === "Session") {
       this.setState({
         remainingMinutes: this.state.breakLength,
@@ -74,14 +77,12 @@ class PomodoroClock extends React.Component {
     }
   }
   incrementBreak() {
+    //ADD ONE TO THE BREAK LENGTH
     let newBreakLength = this.state.breakLength;
-    if (newBreakLength < 60) { newBreakLength++ }; //can't be more than 60
-    this.setState({
-
-        
-      breakLength: newBreakLength
-    })
-    if (this.state.type === "Break") { //if break incremented during break
+    if (newBreakLength < 60) { newBreakLength++ }; //max: 60
+    this.setState( {breakLength: newBreakLength});
+    //IF INCREMENTED DURING BREAK, RESET AND PAUSE
+    if (this.state.type === "Break") { 
       this.setState({
         running: false,
         type: "Break",
@@ -89,16 +90,17 @@ class PomodoroClock extends React.Component {
         remainingSeconds: 0,
       })
     }
+    // UPDATE SIZE OF ICON
     let newSize = (newBreakLength * 5) + "px";
-    document.getElementById("break-icon").style.fontSize = newSize; //change size of icon
+    document.getElementById("break-icon").style.fontSize = newSize; 
   }
   decrementBreak() {
+    //SUBTRACT ONE FROM THE BREAK LENGTH
     let newBreakLength = this.state.breakLength;
-    if (newBreakLength > 1) { newBreakLength-- }; //can't be less than 1
-    this.setState({
-      breakLength: newBreakLength
-    })
-    if (this.state.type === "Break") { //if break decremented during break
+    if (newBreakLength > 1) { newBreakLength-- }; //min: 1
+    this.setState({breakLength: newBreakLength});
+    //IF DECREMENTED DURING BREAK, RESET AND PAUSE
+    if (this.state.type === "Break") { 
       this.setState({
         running: false,
         type: "Break",
@@ -106,15 +108,18 @@ class PomodoroClock extends React.Component {
         remainingSeconds: 0,
       })
     }
+    // UPDATE SIZE OF ICON
     let newSize = (newBreakLength * 5) + "px";
-    document.getElementById("break-icon").style.fontSize = newSize; //change size of icon
+    document.getElementById("break-icon").style.fontSize = newSize; 
   }
   incrementSession() {
+    //ADD ONE TO THE SESSION LENGTH
     let newSessionLength = this.state.sessionLength;
     if (newSessionLength < 60) { newSessionLength++ }; //can't be more than 60
     this.setState({
       sessionLength: newSessionLength
     })
+    //IF INCREMENTED DURING SESSION, RESET AND PAUSE
     if (this.state.type === "Session") {//if session incremented during session
       this.setState({
         running: false,
@@ -123,15 +128,18 @@ class PomodoroClock extends React.Component {
         remainingSeconds: 0,
       })
     }
+    // UPDATE SIZE OF ICON
     let newSize = newSessionLength + "px";
-    document.getElementById("session-icon").style.fontSize = newSize; //change size of icon
+    document.getElementById("session-icon").style.fontSize = newSize; 
   }
   decrementSession() {
+    //SUBTRACT ONE FROM THE SESSION LENGTH
     let newSessionLength = this.state.sessionLength;
     if (newSessionLength > 1) { newSessionLength-- }; //can't be less than 1
     this.setState({
       sessionLength: newSessionLength
     })
+    //IF DECREMENTED DURING SESSION, RESET AND PAUSE
     if (this.state.type === "Session") {//if session decremented during session
       this.setState({
         running: false,
@@ -140,34 +148,37 @@ class PomodoroClock extends React.Component {
         remainingSeconds: 0,
       })
     }
+    // UPDATE SIZE OF ICON
     let newSize = newSessionLength + "px";
-    document.getElementById("session-icon").style.fontSize = newSize; //change size of icon
+    document.getElementById("session-icon").style.fontSize = newSize; 
   }
   reset() {
-    document.getElementById("beep").pause()
-    document.getElementById("beep").currentTime = 0
-    this.setState({
-      running: false,
-      breakLength: 5,
-      sessionLength: 25,
-      type: "Session",
-      remainingMinutes: 25,
-      remainingSeconds: 0
-    })
+    // RETURN SETTINGS TO DEFAULT
+    this.setState(DEFAULT)
+  }
+  padWithZero(num) {
+    // PAD WITH ZERO IN FRONT FOR SINGLE DIGIT TIMES
+    let result = num;
+    if (num < 10) { result = "0" + num.toString() }
+    return result;
   }
   render() {
     return (
 
       <div id="pomodoro-clock-container">
+        {/* DRUMBEAT AUDIO TO DENOTE TRANSITIONS */}
         <audio id="beep" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" controls autoplay></audio>
 
         <h1>Pomodoro Clock</h1>
+        {/* TWO BUBBLES WITH ICONS STRICTLY FOR AESTHETIC APPEAL */}
         <div id="bubbles-container">
           <div id="break-bubble-container"><i id="break-icon" className="fa fa-coffee" style={{ fontSize: 25 }}></i></div>
           <div id="session-bubble-container"><i id="session-icon" className="fa fa-pencil" style={{ fontSize: 25 }}></i></div>
         </div>
-
+        {/* TWO BOXES BELOW BUBBLES DENOTE BREAK LENGTH AND SESSION LENGTH */}
+        {/* BUTTONS CHANGE LENGTHS */}
         <div id="length-boxes-container">
+          {/* LEFT LENGTH BOX */}
           <div className="length-box">
             <div id="break-label" className="smaller-text white-text">Break Length</div>
             <div id="break-length" className="smaller-text boxed">{this.state.breakLength}</div>
@@ -175,9 +186,8 @@ class PomodoroClock extends React.Component {
               <button id="break-decrement" onClick={this.decrementBreak}><i className="fa fa-minus"></i></button>
               <button id="break-increment" onClick={this.incrementBreak}><i className="fa fa-plus"></i></button>
             </div>
-
           </div>
-
+          {/* RIGHT LENGTH BOX */}
           <div className="length-box">
             <div id="session-label" className="smaller-text white-text">Session Length</div>
             <div id="session-length" className="smaller-text boxed">{this.state.sessionLength}</div>
@@ -185,17 +195,16 @@ class PomodoroClock extends React.Component {
               <button id="session-decrement" onClick={this.decrementSession}><i className="fa fa-minus"></i></button>
               <button id="session-increment" onClick={this.incrementSession}><i className="fa fa-plus"></i></button>
             </div>
-
           </div>
         </div>
-
+        {/* DISPLAYS LABEL(SESSION OR BREAK), TIME LEFT, START/PAUSE, AND RESET */}
         <div id="timer-container">
           <div id="timer-container__circle">
             <div id="timer-label" className="larger-text white-text">{this.state.type}</div>
-            <div id="time-left" className="larger-text boxed">{padWithZero(this.state.remainingMinutes)}:{padWithZero(this.state.remainingSeconds)}</div>
+            <div id="time-left" className="larger-text boxed">{this.padWithZero(this.state.remainingMinutes)}:{this.padWithZero(this.state.remainingSeconds)}</div>
             <div id="timer-container__circle__buttons">
               <button id="start_stop" onClick={this.startOrStopSession}><i className="fa fa-play"></i> <i className="fa fa-pause"></i></button>
-              {/* swith to pause icon while running */}
+              {/* switch to pause icon while running */}
               <button id="reset" onClick={this.reset}><i className="fa fa-refresh"></i></button>
             </div>
 
@@ -206,8 +215,5 @@ class PomodoroClock extends React.Component {
     );
   }
 }
-
-
-// ReactDOM.render(<PomodoroClock />, document.getElementById("pomodoro-clock-app"))
 
 export default PomodoroClock;
